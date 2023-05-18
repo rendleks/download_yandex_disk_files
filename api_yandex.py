4a18ea3dcf5ea2998e888b091f18b650a2b96e1c
Aleksandr <0rendl050@gmail.com>
2023-05-18 23:57:50 +0300

    add function download files

--- api_yandex.py
+++ api_yandex.py
@@ -6,13 +6,44 @@ import subprocess
 
 
 def get_href(public_link):
-    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
 
+    base_url = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?'
     final_url = base_url + urlencode(dict(public_key=public_link))
     response = requests.get(final_url)
-    parse_url = response.json()['href']
-    download_url = requests.get(parse_url)
-    print("Скачивание успешно!", public_link)
+    parse_href = response.json()['href']
+    return parse_href
 
 
-get_href('https://disk.yandex.ru/d/74xEHTD3tabF-w')
+def download_files(url):
+    
+    currient_folder = os.getcwd()
+    destination_folder = os.path.join(currient_folder, "download_files")
+
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    start_filename = url.find('filename=')
+    end_filename = url[start_filename:].find('&')
+    end_name = start_filename + end_filename
+    filename = url[start_filename:end_name][9:]
+    download_url = requests.get(url)
+    with open(os.path.join(destination_folder, filename), 'wb') as ff:
+        ff.write(download_url.content)
+    
+    print("Скачан файл: ", filename)
+
+
+def main():
+    #example_link = "https://disk.yandex.ru/d/74xEHTD3tabF-w"
+    #download_files(get_href(example_link))
+    try:
+        with open('list_urls.csv', 'r', encoding='utf-8', newline="") as read_file:
+            reading = csv.reader(read_file)
+            for row in reading:
+                href = str(*row)
+    except FileNotFoundError:
+        print("Файл не найден!")
+
+
+if __name__ == "__main__":
+    main()
